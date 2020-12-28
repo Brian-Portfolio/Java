bb31d4e6e9648c494c5b833b7e96bbec9f9041b4
brian aguilar <brianaguilar@brians-MBP-3.fios-router.home>
2020-12-28 03:31:53 -0500

    Player_Search_App

---
+++ Week2/player_search_app/src/main/java/com/app/dao/impl/PlayerSearchDAOImpl.java
@@ -0,0 +1,306 @@
+package com.app.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.app.dao.PlayerSearchDAO;
+import com.app.dao.dbutil.PostresqlConnection;
+import com.app.exception.BusinessException;
+import com.app.model.Player;
+
+public class PlayerSearchDAOImpl implements PlayerSearchDAO {
+
+	@Override
+	public Player getPlayerById(int player_id) throws BusinessException {
+		Player player = null;
+		try(Connection connection=PostresqlConnection.getConnection()){
+			String sql="select player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender,dob from test.player where player_id=?";
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setInt(1, player_id);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			if (resultSet.next()){
+				player = new Player();
+				player.setPlayer_id(player_id);
+				player.setPlayer_name(resultSet.getString("player_name"));
+				player.setTeam_id(resultSet.getInt("team_id"));
+				player.setPlayer_age(resultSet.getInt("player_age"));
+				player.setState_id(resultSet.getInt("state_id"));
+				player.setJersey_no(resultSet.getInt("jersey_no"));
+				player.setManager_id(resultSet.getInt("manager_id"));
+				player.setPosition_id(resultSet.getInt("position_id"));
+				player.setPlayer_gender(resultSet.getString("player_gender"));
+				player.setDob(resultSet.getString("dob"));
+			}else {
+				throw new BusinessException("No Player found with Id "+player_id);
+			}
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e); //Take off this line when app is live. For error checking 
+			throw new BusinessException("Internal error occurred contact SYSADMIN ");
+		}
+		return player;
+	}
+	
+
+	@Override
+	public Player getPlayerByJerseyNo(int jersey_no) throws BusinessException {
+		Player player = null;
+		try(Connection connection=PostresqlConnection.getConnection()){
+			String sql="select player_id,player_name, team_id, player_age, state_id, manager_id, position_id, player_gender, dob from test.player where jersey_no=?";
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setInt(1, jersey_no);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			if (resultSet.next()){
+				player = new Player();
+				player.setPlayer_id(jersey_no);
+				player.setPlayer_name(resultSet.getString("player_name"));
+				player.setTeam_id(resultSet.getInt("team_id"));
+				player.setPlayer_age(resultSet.getInt("player_age"));
+				player.setState_id(resultSet.getInt("state_id"));
+				player.setJersey_no(resultSet.getInt("player_id"));
+				player.setManager_id(resultSet.getInt("manager_id"));
+				player.setPosition_id(resultSet.getInt("position_id"));
+				player.setPlayer_gender(resultSet.getString("player_gender"));
+				player.setDob(resultSet.getString("dob"));
+			}else {
+				throw new BusinessException("No Player found with Jersey Number "+jersey_no);
+			}
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e); //Take off this line when app is live. For error checking 
+			throw new BusinessException("Internal error occurred contact SYSADMIN ");
+		}
+		return player;
+	}
+	
+
+	@Override
+	public List<Player> getAllPlayers() throws BusinessException {
+		List<Player> playersList=new ArrayList<>(); 
+		try(Connection connection=PostresqlConnection.getConnection()){
+			String sql="select player_id,player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender,dob from test.player";
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){
+				Player player = new Player();
+				player.setPlayer_id(resultSet.getInt("player_id"));
+				player.setPlayer_name(resultSet.getString("player_name"));
+				player.setTeam_id(resultSet.getInt("team_id"));
+				player.setPlayer_age(resultSet.getInt("player_age"));
+				player.setState_id(resultSet.getInt("state_id"));
+				player.setJersey_no(resultSet.getInt("jersey_no"));
+				player.setManager_id(resultSet.getInt("manager_id"));
+				player.setPosition_id(resultSet.getInt("position_id"));
+				player.setPlayer_gender(resultSet.getString("player_gender"));
+				player.setDob(resultSet.getString("dob"));
+				playersList.add(player);
+			}
+			if (playersList.size()==0) {
+				throw new BusinessException("No Players in the DB so far");
+			}
+			
+			
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e); //Take off this line when app is live. For error checking 
+			throw new BusinessException("Internal error occurred contact SYSADMIN ");
+		}
+		return playersList;
+	}
+
+	@Override
+	public List<Player> getPlayersByAge(int player_age) throws BusinessException {
+		List<Player> playersList=new ArrayList<>(); 
+		try(Connection connection=PostresqlConnection.getConnection()){
+			String sql="select player_id,player_name, team_id, state_id, jersey_no, manager_id, position_id, player_gender, dob from test.player where player_age=?";
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setInt(1, player_age);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){
+				Player player = new Player();
+				player.setPlayer_id(resultSet.getInt("player_id"));
+				player.setPlayer_name(resultSet.getString("player_name"));
+				player.setTeam_id(resultSet.getInt("team_id"));
+				player.setPlayer_age(player_age);
+				player.setState_id(resultSet.getInt("state_id"));
+				player.setJersey_no(resultSet.getInt("jersey_no"));
+				player.setManager_id(resultSet.getInt("manager_id"));
+				player.setPosition_id(resultSet.getInt("position_id"));
+				player.setPlayer_gender(resultSet.getString("player_gender"));
+				player.setDob(resultSet.getString("dob"));
+				playersList.add(player);
+			}
+			if (playersList.size()==0) {
+				throw new BusinessException("No Players with age "+player_age);
+			}
+			
+			
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e); //Take off this line when app is live. For error checking 
+			throw new BusinessException("Internal error occurred contact SYSADMIN ");
+		}
+		return playersList;
+	}
+
+
+	@Override
+	public List<Player> getPlayersByGender(String player_gender) throws BusinessException {
+		List<Player> playersList=new ArrayList<>(); 
+		try(Connection connection=PostresqlConnection.getConnection()){
+			String sql="select player_id, player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, dob from test.player where player_gender=?";
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setString(1, player_gender);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){
+				Player player = new Player();
+				player.setPlayer_name("player_gender");
+				player.setPlayer_id(resultSet.getInt("player_id"));
+				player.setTeam_id(resultSet.getInt("team_id"));
+				player.setPlayer_age(resultSet.getInt("player_age"));
+				player.setState_id(resultSet.getInt("state_id"));
+				player.setJersey_no(resultSet.getInt("jersey_no"));
+				player.setManager_id(resultSet.getInt("manager_id"));
+				player.setPosition_id(resultSet.getInt("position_id"));
+				player.setDob(resultSet.getString("dob"));
+
+				playersList.add(player);
+			}
+			if (playersList.size()==0) {
+				throw new BusinessException("No Players in the DB so far");
+			}
+			
+			
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e); //Take off this line when app is live. For error checking 
+			throw new BusinessException("Internal error occurred contact SYSADMIN ");
+		}
+		return playersList;
+	}
+
+	@Override
+	public List<Player> getAllPlayersByTeamId(int team_id) throws BusinessException {
+		List<Player> playersList=new ArrayList<>(); 
+		try(Connection connection=PostresqlConnection.getConnection()){
+			String sql="select player_id,player_name,player_age, state_id, jersey_no, manager_id, position_id, player_gender, dob from test.player where team_id=?";
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setInt(1, team_id);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){
+				Player player = new Player();
+				player.setPlayer_id(resultSet.getInt("player_id"));
+				player.setPlayer_name(resultSet.getString("player_name"));
+				player.setTeam_id(resultSet.getInt("player_age"));
+				player.setPlayer_age(team_id);
+				player.setState_id(resultSet.getInt("state_id"));
+				player.setJersey_no(resultSet.getInt("jersey_no"));
+				player.setManager_id(resultSet.getInt("manager_id"));
+				player.setPosition_id(resultSet.getInt("position_id"));
+				player.setPlayer_gender(resultSet.getString("player_gender"));
+				player.setDob(resultSet.getString("dob"));
+
+				playersList.add(player);
+			}
+			if (playersList.size()==0) {
+				throw new BusinessException("No Players with teamid "+team_id);
+			}
+			
+			
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e); //Take off this line when app is live. For error checking 
+			throw new BusinessException("Internal error occurred contact SYSADMIN ");
+		}
+		return playersList;
+	}
+	
+
+	@Override
+	public List<Player> getAllPlayersByName(String player_name) throws BusinessException {
+			List<Player> playersList=new ArrayList<>(); 
+			try(Connection connection=PostresqlConnection.getConnection()){
+				String sql="select player_id, team_id, state_id, jersey_no, manager_id, position_id, player_age, player_gender, dob from test.player where player_name=?";
+				PreparedStatement preparedStatement=connection.prepareStatement(sql);
+				preparedStatement.setString(1, player_name);
+				ResultSet resultSet=preparedStatement.executeQuery();
+				while(resultSet.next()){
+					Player player = new Player();
+					player.setPlayer_id(resultSet.getInt("player_id"));
+					player.setPlayer_name(player_name);
+					player.setTeam_id(resultSet.getInt("team_id"));
+					player.setPlayer_age(resultSet.getInt("player_age"));
+					player.setState_id(resultSet.getInt("state_id"));
+					player.setJersey_no(resultSet.getInt("jersey_no"));
+					player.setManager_id(resultSet.getInt("manager_id"));
+					player.setPosition_id(resultSet.getInt("position_id"));
+					player.setPlayer_gender(resultSet.getString("player_gender"));
+					player.setDob(resultSet.getString("dob"));
+
+					playersList.add(player);
+				}
+				if (playersList.size()==0) {
+					throw new BusinessException("No Players with name "+player_name);
+				}
+				
+				
+			}catch (ClassNotFoundException | SQLException e) {
+				System.out.println(e); //Take off this line when app is live. For error checking 
+				throw new BusinessException("Internal error occurred contact SYSADMIN ");
+			}
+			return playersList;
+	}
+
+	
+
+	@Override
+	public List<Player> getPlayersByDOB(String dob) throws BusinessException {
+		List<Player> playersList=new ArrayList<>(); 
+		try(Connection connection=PostresqlConnection.getConnection()){
+			String sql="select player_name,player_id, team_id, state_id, jersey_no, manager_id, position_id, player_age, player_gender from test.player where (dob= ????-??-??)";
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setString(1, dob);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){
+				Player player = new Player();
+				player.setPlayer_id(resultSet.getInt("player_id"));
+				player.setDob(dob);
+				player.setTeam_id(resultSet.getInt("team_id"));
+				player.setPlayer_age(resultSet.getInt("player_age"));
+				player.setState_id(resultSet.getInt("state_id"));
+				player.setJersey_no(resultSet.getInt("jersey_no"));
+				player.setManager_id(resultSet.getInt("manager_id"));
+				player.setPosition_id(resultSet.getInt("position_id"));
+				player.setPlayer_gender(resultSet.getString("player_gender"));
+				player.setPlayer_name(resultSet.getString("player_name"));
+
+				playersList.add(player);
+			}
+			if (playersList.size()==0) {
+				throw new BusinessException("No Players with that Date of Birth "+dob);
+			}
+			
+			
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e); //Take off this line when app is live. For error checking 
+			throw new BusinessException("Internal error occurred contact SYSADMIN ");
+		}
+		return playersList;
+	}
+
+
+	@Override
+	public List<Player> getAllPlayersByPositionId(int position_id) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Player> getAllPlayersStateId(int state_id) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
---
+++ Week2/player_search_app/src/main/java/com/app/main/PlayerSearchMain.java
@@ -0,0 +1,197 @@
+package com.app.main;
+
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import com.app.exception.BusinessException;
+import com.app.model.Player;
+import com.app.service.PlayerSearchService;
+import com.app.service.impl.PlayerSearchServiceImpl;
+
+public class PlayerSearchMain {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to PlayerSearch App V1.0");
+		System.out.println("-----------------------------------");
+		int ch=0;
+		PlayerSearchService playerSearchService = new PlayerSearchServiceImpl();
+		do {
+			System.out.println("Player SEARCH MENU");
+			System.out.println("=========================");
+			System.out.println("1)By Id)");
+			System.out.println("2)By Jersey_Number)");
+			System.out.println("3)By Age)");
+			System.out.println("4)By Gender)");
+			System.out.println("5)By TeamId)");
+			System.out.println("6)By Dob)");
+			System.out.println("7)By Player Name)");
+			System.out.println("8)By Show All Players)");
+			System.out.println("9)EXIT)");
+			System.out.println("Please enter appropriate choice between 1-10");
+			try {	
+			ch=Integer.parseInt(sc.nextLine());
+			}catch(NumberFormatException e) {}
+			
+			switch(ch) {
+			case 1:
+				System.out.println("Enter Player Id to get Player Details... ");
+				try {
+					int player_id = Integer.parseInt(sc.nextLine());
+					//code to service 
+					Player player=playerSearchService.getPlayerById(player_id);
+					if (player!=null) {
+						System.out.println("Player found with id: "+player_id);
+						System.out.println(player);
+					}
+					
+				}catch(NumberFormatException e) {
+					System.out.println("Player Id cannot be special characters or symbols or white spaces, it is numeric ");
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				}
+				
+				break;
+				
+			case 2:
+				System.out.println("Enter Jersey Number to obtain matching player");
+				try {
+					int jersey_no = Integer.parseInt(sc.nextLine());
+					//code to service 
+					Player player=playerSearchService.getPlayerById(jersey_no);
+					if (player!=null) {
+						System.out.println("Player found with Jersey_Number: "+jersey_no);
+						System.out.println(player);
+					}
+					
+				}catch(NumberFormatException e) {
+					System.out.println("Jersey Number cannot be special characters or symbols or white spaces, it is numeric ");
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				}
+				
+				break;
+			
+			case 3:
+				System.out.println("Enter Player Age to get all the players matching with that age");
+				
+				try {
+					int player_age=Integer.parseInt(sc.nextLine());
+					List<Player> agePlayersList=playerSearchService.getPlayersByAge(player_age);
+					if(agePlayersList!=null && agePlayersList.size()>0) {
+						System.out.println("There are "+agePlayersList.size()+" no of player/s.. with age "+player_age+" Printing their details......");
+						for(Player p:agePlayersList) {
+							System.out.println(p);
+						}
+					}
+				}catch (NumberFormatException e) {
+					System.out.println("Age cannot have special chars or spaces or alphabets");
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				}
+				
+				break;
+			
+			case 4:
+				System.out.println("Please Enter the Gender of the Player");
+				String player_gender=sc.nextLine();
+				try {
+					List<Player> PlayersGenderList=playerSearchService.getPlayersByGender(player_gender);
+					if(PlayersGenderList!=null && PlayersGenderList.size()>0) {
+						System.out.println("There are "+PlayersGenderList.size()+" no of player/s.. with gender "+player_gender+" Printing their details......");
+						for(Player p:PlayersGenderList) {
+							System.out.println(p);
+						}
+					}
+				
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				} 
+				
+				break;
+				
+			case 5:
+				System.out.println("Please enter Teamid for the matching player");
+				try {
+					int team_id=Integer.parseInt(sc.nextLine());
+					List<Player> teamplayersList=playerSearchService.getAllPlayersByTeamId(team_id);
+					if(teamplayersList!=null && teamplayersList.size()>0) {
+						System.out.println("There are "+teamplayersList.size()+" no of player/s.. with teamid "+team_id+" Printing their details......");
+						for(Player p:teamplayersList) {
+							System.out.println(p);
+						}
+					}
+				}catch (NumberFormatException e) {
+					System.out.println("Teamid cannot have special chars or spaces or alphabets");
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+				
+			case 6:
+				System.out.println("Please enter date of birth of matching player");
+				String dob=sc.nextLine();
+				try {
+					
+					List<Player> PlayersDOBList=playerSearchService.getPlayersByDOB(dob);
+					if(PlayersDOBList!=null && PlayersDOBList.size()>0) {
+						System.out.println("There are "+PlayersDOBList.size()+" no of player/s.. with Date of Birth "+dob+" Printing their details......");
+						for(Player p:PlayersDOBList) {
+							System.out.println(p);
+						}
+					}
+				
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			
+			case 7:
+				System.out.println("Enter PlayerName to get information and stats");
+				String player_name1=sc.nextLine();
+				try {
+					
+					List<Player> NamesPlayersList=playerSearchService.getAllPlayersByName(player_name1);
+					if(NamesPlayersList!=null && NamesPlayersList.size()>0) {
+						System.out.println("There are "+NamesPlayersList.size()+" no of player/s.. with name "+player_name1+" Printing their details......");
+						for(Player p:NamesPlayersList) {
+							System.out.println(p);
+						}
+					}
+				
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				}
+				break; 
+				
+				
+			case 8:
+				System.out.println("Retreiving all the players from backend ");
+				try {
+					List<Player> AllPlayersList=playerSearchService.getAllPlayers();
+					if(AllPlayersList!=null && AllPlayersList.size()>0) {
+						System.out.println("There are "+AllPlayersList.size()+" no of player/s.. Printing their details......");
+						for(Player p:AllPlayersList) {
+							System.out.println(p);
+						}
+					}
+				} catch (BusinessException e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 9:System.out.println("Thank you for using our Player Search App V1.0.. Have a good one...:) ");
+				break;
+			default: System.out.println("Invalid menu option... Kindly Retry...........!!!!");
+				break;
+			
+			}
+
+
+		}while(ch!=9);
+		
+	}
+
+}
