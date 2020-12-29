82b8e894f76222b15c0da99faf1e6473d1bb611d
brian aguilar <brianaguilar@brians-MBP-3.fios-router.home>
2020-12-29 14:45:08 -0500

    SQLScriptsPlayerTable

---
+++ Week2/SQL_Script_Player_DataTable/PlayerDatatable.sql
@@ -0,0 +1,18 @@
+INSERT INTO test.player
+(player_id, player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender, dob)
+VALUES(1, 'Wilson', 100, 33, 301, 56, 1000, 401, 'M', '1991-04-04');
+INSERT INTO test.player
+(player_id, player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender, dob)
+VALUES(2, 'Brady', 101, 42, 302, 84, 1001, 402, 'M', '1986-10-11');
+INSERT INTO test.player
+(player_id, player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender, dob)
+VALUES(3, 'Murray', 102, 30, 303, 60, 1002, 403, 'M', '1889-11-12');
+INSERT INTO test.player
+(player_id, player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender, dob)
+VALUES(4, 'Allen', 103, 26, 304, 30, 1003, 404, 'M', '1993-09-30');
+INSERT INTO test.player
+(player_id, player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender, dob)
+VALUES(5, 'Prescott', 104, 27, 305, 20, 1004, 405, 'M', '1995-05-23');
+INSERT INTO test.player
+(player_id, player_name, team_id, player_age, state_id, jersey_no, manager_id, position_id, player_gender, dob)
+VALUES(6, 'Smith', 105, 38, 306, 47, 1005, 406, 'M', '1985-03-28');
